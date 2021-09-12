d3c3b1b1c3b8aeddf21a08cd3467ee8b88211aff
Chiara <csabato@unc.edu>
2021-09-12 13:16:49 -0400

    progress on ex03

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,21 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730466575"
+
+word: str = input("Enter a word: ")
+i: int = 0
+counter: int = 0
+
+while i < len(word):
+    j: int = i + 1
+    character: str = word[i]
+    while j < len(word) - 1:
+        if character == word[j]:
+            counter += 1
+        j += 1
+    i += 1
+
+if counter > 0:
+    print("True")
+else:
+    print("False")
---
+++ lessons/functions.py
@@ -0,0 +1,6 @@
+def your_mom(a: int, b: str):
+    return str(a) + b
+
+
+answer: str = your_mom(5, "ciao")
+print(answer)
