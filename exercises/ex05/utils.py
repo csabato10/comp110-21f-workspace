0f820408bb36feffb9bd33c57e08817313a4f85f
Chiara <csabato@unc.edu>
2021-10-01 17:39:48 -0400

    progress on ex05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -8,6 +8,7 @@ __author__ = "730466575"
 
 
 def only_evens(numbs: list[int]) -> list[int]:
+    """Returns only even numbers of a list."""
     evens: list[int] = []
     for numb in numbs:
         if numb % 2 == 0:
@@ -19,8 +20,9 @@ def only_evens(numbs: list[int]) -> list[int]:
 
 
 def sub(b: list[int], start: int, end: int) -> list[int]:
+    """Returns list values at given indices."""
     indexed_list: list[int] = []
-    if len(b) == 0 or start > len(b) or end <= 0:
+    if len(b) == 0 or start >= len(b) or end <= 0:
         return indexed_list
     
     if start <= 0:
@@ -39,6 +41,7 @@ def sub(b: list[int], start: int, end: int) -> list[int]:
 
 
 def concat(a: list[int], b: list[int]) -> list[int]:
+    """Combines two lists into one."""
     new_list: list[int] = []
     for value in a:
         new_list.append(value)
@@ -46,7 +49,3 @@ def concat(a: list[int], b: list[int]) -> list[int]:
         new_list.append(value)
     return new_list
     
-
-print(only_evens([2, 5, 6]))
-print(sub([30, 49, 23, 45], 0, 0))
-print(concat([4, 5, 6], [7, 3, 8]))
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -7,21 +7,25 @@ __author__ = "730466575"
 
 
 def test_only_evens_zeros() -> None:
+    """Test only evens: zero values."""
     numbs: list[int] = [0, 0, 0]
     assert only_evens(numbs) == [0, 0, 0]
 
 
 def test_only_evens_one_number() -> None:
+    """Test only evens: one integer number"""
     numbs: list[int] = [2, 2, 2]
     assert only_evens(numbs) == [2, 2, 2]
 
 
 def test_only_evens_some_even() -> None:
+    """Test only evens: some even values."""
     numbs: list[int] = [2, 5, 8]
     assert only_evens(numbs) == [2, 8]
 
 
 def test_sub_empty_list() -> None:
+    """Test sub: empty list."""
     b: list[int] = []
     start: int = 1
     end: int = 3
@@ -29,6 +33,7 @@ def test_sub_empty_list() -> None:
 
 
 def test_sub_negative_start() -> None:
+    """Test sub: negative start index."""
     b: list[int] = [2, 3, 4, 5]
     start: int = -3
     end: int = 2
@@ -36,6 +41,7 @@ def test_sub_negative_start() -> None:
 
 
 def test_sub_normal_list() -> None:
+    """Test sub: normal list."""
     b: list[int] = [2, 3, 4, 5]
     start: int = 1
     end: int = 2
@@ -43,6 +49,7 @@ def test_sub_normal_list() -> None:
 
 
 def test_sub_negative_end() -> None:
+    """Test sub: negative end index."""
     b: list[int] = [2, 3, 4, 5]
     start: int = 1
     end: int = -2
@@ -50,6 +57,7 @@ def test_sub_negative_end() -> None:
 
 
 def test_sub_same_large_end() -> None:
+    """Test sub: end value out of string length."""
     b: list[int] = [2, 3, 4, 5]
     start: int = 1
     end: int = 8
@@ -57,24 +65,28 @@ def test_sub_same_large_end() -> None:
 
 
 def test_concat_random_numbers() -> None:
+    """Test concat: normal values."""
     a: list[int] = [1, 3, 5]
     b: list[int] = [8, 9, 2]
     assert concat(a, b) == [1, 3, 5, 8, 9, 2]
 
 
 def test_concat_empty_lists() -> None:
+    """Test concat: empty list."""
     a: list[int] = []
     b: list[int] = []
     assert concat(a, b) == []
 
 
 def test_concat_patterened_numbers() -> None:
+    """Test concat: patterend numbers."""
     a: list[int] = [1, 5, 1]
     b: list[int] = [5, 1, 5]
     assert concat(a, b) == [1, 5, 1, 5, 1, 5]
 
 
 def test_concat_zeros() -> None:
+    """Test concat: lists of zeros."""
     a: list[int] = [0, 0, 0]
     b: list[int] = [0, 0, 0]
     assert concat(a, b) == [0, 0, 0, 0, 0, 0]
