a9b63bc818ae2f1c17bfd434b324127616ca909e
Chiara <csabato@unc.edu>
2021-10-01 17:31:58 -0400

    progress on ex05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,52 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730466575"
 
 # Define your functions below
+
+"""Only evens."""
+
+
+def only_evens(numbs: list[int]) -> list[int]:
+    evens: list[int] = []
+    for numb in numbs:
+        if numb % 2 == 0:
+            evens.append(numb) 
+    return evens
+
+
+"""Sub."""
+
+
+def sub(b: list[int], start: int, end: int) -> list[int]:
+    indexed_list: list[int] = []
+    if len(b) == 0 or start > len(b) or end <= 0:
+        return indexed_list
+    
+    if start <= 0:
+        indexed_list.append(b[0])
+    else:
+        indexed_list.append(b[start])
+    
+    if end >= len(b):
+        indexed_list.append(b[-1])
+    else:
+        indexed_list.append(b[end])
+    return indexed_list
+
+
+"""Concat."""
+
+
+def concat(a: list[int], b: list[int]) -> list[int]:
+    new_list: list[int] = []
+    for value in a:
+        new_list.append(value)
+    for value in b:
+        new_list.append(value)
+    return new_list
+    
+
+print(only_evens([2, 5, 6]))
+print(sub([30, 49, 23, 45], 0, 0))
+print(concat([4, 5, 6], [7, 3, 8]))
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,80 @@
 """Unit tests for list utility functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+
+__author__ = "730466575"
+
+
+def test_only_evens_zeros() -> None:
+    numbs: list[int] = [0, 0, 0]
+    assert only_evens(numbs) == [0, 0, 0]
+
+
+def test_only_evens_one_number() -> None:
+    numbs: list[int] = [2, 2, 2]
+    assert only_evens(numbs) == [2, 2, 2]
+
+
+def test_only_evens_some_even() -> None:
+    numbs: list[int] = [2, 5, 8]
+    assert only_evens(numbs) == [2, 8]
+
+
+def test_sub_empty_list() -> None:
+    b: list[int] = []
+    start: int = 1
+    end: int = 3
+    assert sub(b, start, end) == []
+
+
+def test_sub_negative_start() -> None:
+    b: list[int] = [2, 3, 4, 5]
+    start: int = -3
+    end: int = 2
+    assert sub(b, start, end) == [2, 4]
+
+
+def test_sub_normal_list() -> None:
+    b: list[int] = [2, 3, 4, 5]
+    start: int = 1
+    end: int = 2
+    assert sub(b, start, end) == [3, 4]
+
+
+def test_sub_negative_end() -> None:
+    b: list[int] = [2, 3, 4, 5]
+    start: int = 1
+    end: int = -2
+    assert sub(b, start, end) == []
+
+
+def test_sub_same_large_end() -> None:
+    b: list[int] = [2, 3, 4, 5]
+    start: int = 1
+    end: int = 8
+    assert sub(b, start, end) == [3, 5]
+
+
+def test_concat_random_numbers() -> None:
+    a: list[int] = [1, 3, 5]
+    b: list[int] = [8, 9, 2]
+    assert concat(a, b) == [1, 3, 5, 8, 9, 2]
+
+
+def test_concat_empty_lists() -> None:
+    a: list[int] = []
+    b: list[int] = []
+    assert concat(a, b) == []
+
+
+def test_concat_patterened_numbers() -> None:
+    a: list[int] = [1, 5, 1]
+    b: list[int] = [5, 1, 5]
+    assert concat(a, b) == [1, 5, 1, 5, 1, 5]
+
+
+def test_concat_zeros() -> None:
+    a: list[int] = [0, 0, 0]
+    b: list[int] = [0, 0, 0]
+    assert concat(a, b) == [0, 0, 0, 0, 0, 0]
