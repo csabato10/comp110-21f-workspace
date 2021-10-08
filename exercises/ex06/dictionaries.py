706ab25a979b185dc52ea4bb859b7b76bcd6e84c
Chiara <csabato@unc.edu>
2021-10-08 13:35:09 -0400

    progress on ex06

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,50 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730466575"
 
 # Define your functions below
+
+
+def invert(a: dict[str, str]) -> dict[str, str]:
+    new_dictionary: dict[str, str] = {}
+    for key in a:
+        check_key: str = a[key]
+        count: int = 0
+        for key in a:
+            if a[key] == check_key:
+                count += 1
+        if count > 1:
+            raise KeyError("Two of the same key.")
+    for key in a:
+        new_dictionary[key] = key
+    return new_dictionary
+
+
+
+
+def favorite_color(colors: dict[str, str]) -> str:
+    tally: dict[str, int] = {}
+    for name in colors:
+        count: int = 0
+        color: str = colors[name]
+        for name in colors:
+            if color == colors[name]:
+                count += 1
+        tally[color] = count
+    popular_color: str
+    votes: int = 0
+    for color in tally:
+        if tally[color] > votes:
+            popular_color = color
+            votes = tally[color]
+    return popular_color
+
+
+def count(class_schedule: list[str]) -> dict[str, int]:
+    final_dict: dict[str, int] = {}
+    for value in class_schedule:
+        if value in final_dict:
+            final_dict[value] += 1
+        else:
+            final_dict[value] = 1
+    return final_dict
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,52 @@
 """Unit tests for dictionary functions."""
+ 
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+
+__author__ = "730466575"
+
+
+def test_same_key_in_invert() -> None:
+    a = {"string": "bean", "pinto": "bean"}
+    assert invert(a) == KeyError("Two of the same key.")
+
+
+def test_phrase_invert() -> None:
+    a = {"hello": "there", "my": "friend"}
+    assert invert(a) == {"there": "hello", "friend": "my"}  
+
+
+def test_normal_invert() -> None:
+    a = {"string": "bean", "blue": "bird"}
+    assert invert(a) == {"bean": "string", "bird": "blue"}  
+
+
+def test_all_different_colors() -> None:
+    colors = {"Karl": "orange", "Daniel": "red", "James": "purple"}
+    assert favorite_color(colors) == "orange"
+
+
+def test_tie_colors() -> None:
+    colors = {"Karl": "purple", "Daniel": "orange", "James": "purple", "Blake": "orange"}
+    assert favorite_color(colors) == "purple"
+
+
+def test_singal_popular_colors():
+    colors = {"Karl": "red", "Daniel": "red", "James": "purple"}
+    assert favorite_color(colors) == "red"
+
+
+def test_all_different_count():
+    classes = ["math", "science", "english"]
+    assert count(classes) == {"math": 1, "science": 1, "english": 1}
+
+
+def test_repeated_classes_count():
+    classes = ["math", "science", "english", "science"]
+    assert count(classes) == {"math": 1, "science": 2, "english": 1}
+
+
+def test_no_classes_count():
+    classes = []
+    assert count(classes) == {}
