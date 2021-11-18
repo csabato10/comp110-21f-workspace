d13a8aca9e07f7be4790637896663770ac4f633c
Chiara <csabato@unc.edu>
2021-11-18 12:16:59 -0500

    .

--- exercises/ex08/Simpy.py
+++ exercises/ex08/Simpy.py
@@ -4,10 +4,15 @@ from __future__ import annotations
 
 from typing import Union
 
-__author__ = "YOUR PID HERE"
+__author__ = "730466575"
 
 
 class Simpy:
     values: list[float]
 
     # TODO: Your constructor and methods will go here.
+
+    def __init__(self, nums: list[float]) -> None:
+        self.values = nums
+
+        
