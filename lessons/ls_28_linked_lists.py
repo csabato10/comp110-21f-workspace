3f8d9ca1375617beabc8c5de0fedbc9dc4a844f3
Chiara <csabato@unc.edu>
2021-11-24 15:10:13 -0500

    progress on lesson 28

---
+++ lessons/ls_28_linked_lists.py
@@ -0,0 +1,29 @@
+"""Simply linked lists."""
+
+from __future__ import annotations
+
+from typing import Optional
+
+
+class Node:
+    """Single linked list Node."""
+    data: int
+    next: Optional[Node]
+
+    def __init__(self, data: int, next: Optional[Node]):
+        """Constructor."""
+        self.data = data 
+        self.next = next
+
+
+def count(head: Optional[Node]) -> int:
+    """Counts up the length of a linked list."""
+    if head is None:
+        return 0
+    else:
+        return 1 + count(head.next)
+
+
+third_node: Node = Node(3, None)
+second_Node = Node(2, third_node)
+a_node: Node = Node(1, second_Node)
